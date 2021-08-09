1f3596b94471d59fa551ca5a973ba8c004c29738
Stephan Klinger <staeff@arcor.de>
2021-08-09 14:38:44 +0200

    Create display component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
+const Display = ({value}) => <div>{value}</div>
+
 const App = () => {
   const [value, setValue] = useState(10)
 
@@ -19,7 +21,7 @@ const App = () => {
       <button onClick={hello('react')}>React</button>
       <button onClick={hello('function')}>Function</button>
       <h2>Parameterized setting of values</h2>
-      {value} <br />
+      <Display value={value} />
       <Button handleClick={() => setToValue(1000)} text="thousand" />
       <Button handleClick={() => setToValue(0)} text="reset" />
       <Button handleClick={() => setToValue(value + 1)} text="increment" />
