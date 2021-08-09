2506f1a5fba2541c6d7232ccc79e08dc8a298349
Stephan Klinger <staeff@arcor.de>
2021-08-09 14:20:19 +0200

    New example for parameterized higher order func

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ const App = () => {
   // function hello returns a function, that can be parameterized
   const hello = (who) => () => console.log('hello', who)
 
+  const setToValue = (newValue) => () => setValue(newValue)
+
   return (
     <div>
-      {value} <br />
+      <h2>Parameterized logging</h2>
       <button onClick={hello('world')}>World</button>
       <button onClick={hello('react')}>React</button>
       <button onClick={hello('function')}>Function</button>
+      <h2>Parameterized setting of values</h2>
+      {value} <br />
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
