f6ebe02c4d74ea0df94b1f187821dca5bd6b40a7
Stephan Klinger <staeff@arcor.de>
2021-08-09 13:58:25 +0200

    Implement event handler as higher order function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, {useState} from 'react'
 
 const App = () => {
+  const [value, setValue] = useState(10)
 
+  // function hello returns a function
+  // so onClick={hello()} renders to onClick{() => console.log('hello world')}
+  const hello = () => {
+      const handler = () => console.log('hello world')
+      return handler
+  }
 
   return (
     <div>
+      {value} <br />
+      <button onClick={hello()}>Click me</button>
     </div>
   )
 }
